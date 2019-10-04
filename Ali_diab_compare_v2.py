e47b14c8b062437bc3898c197f556a16b150845b
Shivam Prasad <shivam13juna@gmail.com>
2019-10-04 18:59:12 +0530

    Yet to finalize code

--- Ali_diab_compare_v2.py
+++ Ali_diab_compare_v2.py
@@ -1,23 +1,18 @@
-
 import numpy as np
 import pandas as pd
 
-
-# ### The goals is to check if phone number match in the field 'estate_phone ' of dupro.csv and cent.csv and to create another file.csv with only the item of dupro.csv without the any item of the matching of ph-one n-umber but be careful because we can have in each field 2,3 or more number. We need to check each one with each one. If a number match we don't take the line. We keep only the line who don't any number of cent.csv doesn't match
-
-
-
-new_dupro = pd.read_csv('avppdupro.csv')
+### The goals is to check if phone number match in the field 'estate_phone ' of dupro.csv and cent.csv and to create another file.csv with only the item of dupro.csv without the any item of the matching of ph-one n-umber but be careful because we can have in each field 2,3 or more number. We need to check each one with each one. If a number match we don't take the line. We keep only the line who don't any number of cent.csv doesn't match
+new_dupro = pd.read_csv('AVPP_TEL.csv')
 new_dupro.dropna(subset=['estate_phone'], inplace=True)
 new_dupro['estate_phone'].fillna(0, inplace=True)
 
-base_list = list(map(lambda x: x.split(','), new_dupro['estate_phone'].astype(str).values))
+base_list = list(map(lambda x: x.split(','), new_dupro['estate_phone'].astype(str)))
 
 for i in range(len(base_list)):
     while '' in base_list[i]:
         base_list[i].remove('')
 
-base_list = [list(map(lambda x : int(float(lst)), lst)) for lst in base_list]
+base_list = [list(map(float, map(int,i))) for i in base_list]
 
 s_list = []
 common = []
@@ -26,10 +21,10 @@ for i in range(len(base_list)):
     for k in range(len(base_list)):
         for j in range(len(base)):
             if base[j] in base_list[k]:
-                common.append(k)
+                common.append(k+1)
        
     common = list(set(common))
-    common.remove(i)
+    common.remove(i+1)
     if len(common) == 0:
         s_list.append('')
     else:
