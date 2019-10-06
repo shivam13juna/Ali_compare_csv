e6be1d6cc8c43bc368f260b7dcea6f85a065070b
shivam13juna <shivam13juna@gmail.com>
2019-10-06 11:58:04 +0530

    I'd like to think it's done

--- .ipynb_checkpoints/Ali_diab_compare_v2-checkpoint.py
+++ .ipynb_checkpoints/Ali_diab_compare_v2-checkpoint.py
@@ -1,19 +1,21 @@
 import numpy as np
 import pandas as pd
 
-### The goals is to check if phone number match in the field 'estate_phone ' of dupro.csv and cent.csv and to create another file.csv with only the item of dupro.csv without the any item of the matching of ph-one n-umber but be careful because we can have in each field 2,3 or more number. We need to check each one with each one. If a number match we don't take the line. We keep only the line who don't any number of cent.csv doesn't match
-new_dupro = pd.read_csv('AVPP_TEL.csv')
-new_dupro.dropna(subset=['estate_phone'], inplace=True)
+#Importing dataset
+new_dupro = pd.read_csv('AVPP_immo.csv')
 new_dupro['estate_phone'].fillna(0, inplace=True)
 
 base_list = list(map(lambda x: x.split(','), new_dupro['estate_phone'].astype(str)))
 
+#Removing all blank characters in numbers
 for i in range(len(base_list)):
     while '' in base_list[i]:
         base_list[i].remove('')
 
+#Converting all numbers from string format to float
 base_list = [list(map(float, map(int,i))) for i in base_list]
 
+# Making set of all the shared numbers in the dataframe
 s_list = []
 common = []
 for i in range(len(base_list)):
@@ -21,22 +23,40 @@ for i in range(len(base_list)):
     for k in range(len(base_list)):
         for j in range(len(base)):
             if base[j] in base_list[k]:
-                common.append(k+1)
+                common.append(k)
        
     common = list(set(common))
-    common.remove(i+1)
     if len(common) == 0:
         s_list.append('')
     else:
         s_list.append(common)
     common = []
 
-# final_list = [list(map(int, lst)) for lst in s_list]
+    
+safe = s_list.copy()
 
+c_list = safe.copy()
+s_list = safe.copy()
 
-new_dupro['similarity_column'] = s_list
-new_dupro['similarity_column'] = new_dupro['similarity_column'].str[0]
-new_dupro.to_csv('new_dupro_v2.csv', index=False)
+#Now assigning a unique id to all the rows based on length. As only the rows that share number with some other row have length of s_list greater than 1
+count = 1000
+visited = set()
 
+for i in range(len(s_list)):
+    if i in visited:
+        pass
+    else:
+        if len(s_list[i])>1:
+            for j in range(len(s_list[i])):
+                c_list[s_list[i][j]] = count
+                visited.add(s_list[i][j])
+            count+=1
+        else:
+            c_list[i] = ''
+
+# Just making a new column with the new c_list as similarity_column
+new_dupro['similarity_column'] = c_list
+new_dupro['similarity_column'] = new_dupro['similarity_column']
+new_dupro.to_csv('new_dupro_v2.csv')
 
 
